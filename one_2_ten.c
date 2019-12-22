01c58a3e54ff4bd18b4c3b4749b1079244db6d4d
asinhacode <57113053+asinhacode@users.noreply.github.com>
2019-12-22 10:04:57 -0500

    signed vs unsigned

--- one_2_ten.c
+++ one_2_ten.c
@@ -27,4 +27,19 @@ int main(int argc, const char * argv[]) {
     
     printf("%d\n", INT_MIN); // OBTAINED BY USING <LIMITS.H>
 
+    
+    // moving it on the screen
+    printf("%s\n", "Hello");
+    printf("%10s\n", "Hello");
+    
+    // char data type
+    char c = 50;
+    printf("%c\n", c);
+    printf("%d\n", c);
+    
+    // signed vs unsigned
+    unsigned i = 1;
+    int j = -4;
+    printf("%d\n", i + j);
+    printf("%u\n", i + j); // convert into 1s complement
 }
